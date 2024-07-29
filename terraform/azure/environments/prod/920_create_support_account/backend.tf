eeca9eaa23895269ed6c79170914d986b08351d1
RXE-00008-KawashimaNarumi <152465594+RXE-00008-KawashimaNarumi@users.noreply.github.com>
2024-07-29 17:25:59 +0900

    Update backend.tf

--- terraform/azure/environments/prod/920_create_support_account/backend.tf
+++ terraform/azure/environments/prod/920_create_support_account/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform_setup"
-    storage_account_name = "XXXXXXXXXX"
+    storage_account_name = "nricspmnghogehoge"
     container_name       = "tfstate"
     use_oidc             = true
     client_id            = "247f9c9d-6dd9-4e08-94a8-513db72194e9"
